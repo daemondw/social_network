3f572298c006034233e8f30f327f43b28aa8c080
Dima Antonenko <daemondw@gmail.com>
2020-08-27 09:33:46 +0200

    social network react app

---
+++ src/components/Music/Music.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import s from './Settings.module.css';
+
+const Music = (props) => {
+    return (
+        <div>
+            Music
+        </div>
+    )
+}
+
+export default Music;
