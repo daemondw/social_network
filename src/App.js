1c4a85f00ddfc4554f6b7b606f4d90dcd196b00e
Dima Antonenko <daemondw@gmail.com>
2020-09-09 12:01:09 +0200

    checking components

--- src/App.js
+++ src/App.js
@@ -4,28 +4,21 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route} from "react-router-dom";
 import News from "./components/News/News";
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
+import {BrowserRouter, Route} from "react-router-dom";
 
 const App = (props) => {
 
     return (
-        <BrowserRouter>
             <div className='app-wrapper'>
                 <Header/>
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path='/dialogs' render={ () => <Dialogs state={props.state.dialogsPage} /> }/>
-                    <Route path='/profile' render={ () => <Profile state={ props.state.profilePage }/> }/>
+                    <Route path='/profile' render={ () => <Profile state={props.state.profilePage}/> }/>
                     <Route path='/news' render={ () => <News /> }/>
-                    <Route path='/music' render={ () => <Music /> }/>
-                    <Route path='/settings' render={ () => <Settings /> }/>
                 </div>
-            </div>
-            </BrowserRouter>
-            )
+            </div>)
 }
 
 export default App;
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -6,7 +6,7 @@ const Post = (props) => {
     return (
         <div className={s.item}>
           <img src="https://s3.amazonaws.com/liberty-uploads/wp-content/uploads/sites/1218/2015/09/avatarsucks.jpg" alt=""/>
-          {props.massege}
+          {props.message}
           <div>
             <span>like</span> {props.likesCount}
           </div>
