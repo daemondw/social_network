99a5cbb839cd55ab404b0552472d78024e7ed943
Dima Antonenko <daemondw@gmail.com>
2020-11-11 14:56:19 +0100

    fix

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,4 @@
+import {rerenderEntireTree} from "../render";
 
 let state = {
     profilePage: {
@@ -38,6 +39,7 @@ export let addPost = (postMessage) => {
         likesCount: 0
     };
     state.profilePage.posts.push(newPost);
+    rerenderEntireTree(state);
 }
 
 export default state;
